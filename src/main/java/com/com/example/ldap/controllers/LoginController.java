25e635799c1fb739ccaf5faf3469164c090617f9
IlyaToporovskiy <lelikleti0@gmail.com>
2019-06-13 01:16:42 +0300

    Initial commit

---
+++ src/main/java/com/com/example/ldap/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.com.example.ldap.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public ModelAndView Login(){
+        return new ModelAndView("login");
+    }
+}
